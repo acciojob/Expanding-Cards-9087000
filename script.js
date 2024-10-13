3d52d8dace8b3a9fa3cb6c195ae310e2ada5a169
9087000 <50361103+9087000@users.noreply.github.com>
2024-10-13 21:22:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,51 +1,11 @@
-*{
-    padding: 0;
-    margin: 0;
-    box-sizing: border-box;
-}
-body {
-    font-family: Arial, sans-serif;
-    height: 100vh;
-    width: 100vw;
-    display: flex;
-    justify-content: center;
-}
-.container {
-    display: flex;
-    justify-content: center;
-    align-self: center;
-    width: 90%;
-    overflow: hidden;
-    gap: 10px;
-}
-.panel {
-    /* flex: 1; */
-    display: flex;
-    flex-direction: column;
-    overflow: hidden;
-    border-radius: 15px;
-    transition: flex 0.5s ease-in-out;
-}
-.panel img {
-    width: 100%;
-    border-radius: 15px;
-    height: 100%;
-    object-fit: cover;
-}
-.panel.active {
-    flex: 5;
-    .img-text {
-        opacity: 1;
-        position: relative;
-        color: rgb(255, 255, 255);
-        bottom: 25px;
-        left: 10px;
-    }
-}
-.panel:not(.active) {
-    flex: 1;
-}
+let cards = document.querySelectorAll(".panel");
+let previous = document.querySelector(".active");
 
-.img-text {
-    opacity: 0;
+for (let card of cards) {
+    card.addEventListener("mouseover", (e) => {
+        console.log(`Previous panel: ${previous.querySelector('.img-text').innerText}`);
+        previous.className = "panel";
+        card.className = "panel active";
+        previous = card;
+    });
 }
