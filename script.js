9c88bb45e1c3a90428736e6e9b283d340bf505ea
9087000 <50361103+9087000@users.noreply.github.com>
2024-10-13 21:00:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,51 @@
-//your JS code here. If required.
-
-const panels = document.querySelectorAll(".panel")
-
-function removeActive() {
-	 panels.forEach(panel => {
-		 panel.classList.remove("active")
-	 })
+*{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
 }
-    panels.forEach(panel => {
-		panel.addEventListener("click",()=>{
-			removeActive()
-		panel.classList.add("active")
-		})
-		
-	})
+body {
+    font-family: Arial, sans-serif;
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    justify-content: center;
+}
+.container {
+    display: flex;
+    justify-content: center;
+    align-self: center;
+    width: 90%;
+    overflow: hidden;
+    gap: 10px;
+}
+.panel {
+    /* flex: 1; */
+    display: flex;
+    flex-direction: column;
+    overflow: hidden;
+    border-radius: 15px;
+    transition: flex 0.5s ease-in-out;
+}
+.panel img {
+    width: 100%;
+    border-radius: 15px;
+    height: 100%;
+    object-fit: cover;
+}
+.panel.active {
+    flex: 5;
+    .img-text {
+        opacity: 1;
+        position: relative;
+        color: rgb(255, 255, 255);
+        bottom: 25px;
+        left: 10px;
+    }
+}
+.panel:not(.active) {
+    flex: 1;
+}
+
+.img-text {
+    opacity: 0;
+}
